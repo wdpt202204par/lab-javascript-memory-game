284348b70a59938509a40056ffd3ed872e3c0ebd
Benjamin Amzzough <amzzough.benjamin@yahoo.fr>
2022-05-21 15:26:35 +0200

    Itération1 ok

--- js/memory.js
+++ js/memory.js
@@ -1,13 +1,24 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
     // add the rest of the class properties here
   }
 
   shuffleCards() {
+    let shuffleCardsArr = [];
+    for (let i = 0; i < this.cards.length; i++) {
+      let j = Math.floor(Math.random() * (i + 1));
+
+      shuffleCardsArr.push(this.cards[j])
+      console.log('hey', i)
+      console.log('hello', shuffleCardsArr)
+    }
+    return shuffleCardsArr
     // ... write your code here
   }
-
   checkIfPair(card1, card2) {
     // ... write your code here
   }
