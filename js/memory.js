28208a7aec238562b7db2db4a6e25953abcbb248
unknown <Jossuha@hotmail.fr>
2022-05-21 17:10:22 +0200

    Memory.js done

--- js/memory.js
+++ js/memory.js
@@ -1,19 +1,53 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
     // add the rest of the class properties here
   }
 
   shuffleCards() {
-    // ... write your code here
+    //   if (this.cards === undefined){
+    //     return undefined
+    //   }
+
+    // let numberOfcards = this.cards.length
+    // for (let i = 0; i < numberOfcards; i++){
+    //   let random = Math.floor(Math.random() * this.cards.length)
+    //   this.pickedCards.push(this.cards[random])
+    //   this.cards.splice(random,1)
+    // }
+
+    // trier this.cards de maniere aleatoire ie: melanger
+    this.cards.sort(function (a, b) {
+      //return >0 / <0 / 0
+      return Math.random() -.5 // ]0;1[ -.5= ]-.5;.5[
+    })
   }
 
   checkIfPair(card1, card2) {
     // ... write your code here
+    this.pairsClicked++
+    if (card1 === card2){
+      this.pairsGuessed++
+      return true
+    }else{
+      return false
+    }
+
   }
 
   checkIfFinished() {
     // ... write your code here
+
+    console.log("picked card = ", this.pickedCards.length)
+    if (this.pairsGuessed === this.cards.length / 2){
+      console.log("finish")
+      return true
+    } else{
+      return false
+    }
   }
 }
 
