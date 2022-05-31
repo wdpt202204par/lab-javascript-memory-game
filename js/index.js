460fdffa73af13bea74f67d593b58936c698dff3
Cécile Deshayes-Guinard <cecile.deshayes.guinard@gmail.com>
2022-06-01 00:22:12 +0200

    done

--- js/index.js
+++ js/index.js
@@ -25,27 +25,57 @@ const cards = [
   { name: 'thor', img: 'thor.jpg' }
 ];
 
-const memoryGame = new MemoryGame(cards);
+const memoryGame = new MemoryGame(cards); // le memory game recommence après que le constructor du fichier memory.js ait redémarré
 
-window.addEventListener('load', (event) => {
-  let html = '';
-  memoryGame.cards.forEach((pic) => {
+window.addEventListener('load', (event) => { 
+
+  memoryGame.shuffleCards() // première étape : on mélange les cartes
+
+  let html = ''; // on déclare une variable html sans valeur pour qu'on le caractériser par la suite
+
+  memoryGame.cards.forEach((pic) => { // pour chaque objet je créé un emplacement HTML par class qui sera pousser dans mon document
     html += `
       <div class="card" data-card-name="${pic.name}">
         <div class="back" name="${pic.img}"></div>
         <div class="front" style="background: url(img/${pic.img}) no-repeat"></div>
       </div>
     `;
-  });
+  }); // "memoryGame.cards" fait appel à mon constructeur (donc à mon statut de démarrage)
 
-  // Add all the divs to the HTML
-  document.querySelector('#memory-board').innerHTML = html;
+  // dans window, nous devons marquer l'emplacement du code HTML produit par notre function addEventListener
+  document.querySelector('#memory-board').innerHTML = html; // ceci représente l'id dans lequel sera pousser le code HTML de ma function forEach
 
-  // Bind the click event of each element to a function
-  document.querySelectorAll('.card').forEach((card) => {
+  let clickOnCard = undefined; // on click sur xCard
+
+  // ici, nous devons caractériser ce que provoque le click
+  document.querySelectorAll('.card').forEach((card) => { // dans toute la class "card" pour chaque carte
     card.addEventListener('click', () => {
-      // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
+
+      if (clickOnCard === undefined) { // si je clique sur une carte indéfinie
+        clickOnCard = card; // cette carte devient la variable card
+        card.classList.add("turned")  // on ajoute un effet àla class card 
+        } else {
+        memoryGame.checkIfPair() // si par contre la variable card est déjà définie par un premier click, alors je teste ma function checkIfPair
+      }
+
+
+      setTimeout(() => { 
+      if (checkIfPair() === true) { // si mon test de compatibilité est vrai alors...
+        card.classList.add("turned") // je retourne la carte cliquée
+        memoryGame.pairsClicked()++ // j'ajoute un test validé à pairsClicked qui est parti de 0 dans mon constructor
+        } else {
+        card.classList.remove("turned") // si ça ne se vérifie pas, j'enlève les cartes retournées.
+        }
+      
+
+      memoryGame.checkIfFinished() // si j'ai trouvé toute les cartes...
+      document.querySelector("#score #pairs-guessed").innerHTML = memoryGame.pairsGuessed // je pousse le résultat de mon this paisGuessed dans l'HTML du document
+
+      console.log(`Card clicked: ${card}`); // je print la carte cliquée
+      },1000);
+
+      document.querySelector("#score #pairs-clicked").innerHTML = memoryGame.pairsClicked
     });
   });
 });
+
--- js/memory.js
+++ js/memory.js
@@ -1,43 +1,45 @@
-class MemoryGame {
+class MemoryGame { // On doit créer une class qui représente la méthode du jeu
   constructor(cards) {
-    this.cards = cards;
-    // add the rest of the class properties here
-    this.pickedCards = []
-    this.pairsClicked = 0;
-    this.pairsGuessed = 0;
-  }
+    this.cards = cards; // métode générale pour créer le constructor. La méthode this.cards concerne tous les éléments de la variable cards dans index.js
+    this.pickedCards = []; // on prend les cartes du tableau
+    this.pairsClicked = 0; // on commence avec 0 cartes cliquées
+    this.pairsGuessed = 0; // on commence avec 0 cartes trouvées
+  };
+
 
   shuffleCards() {
-    // ... write your code here
-    this.cards.sort(function (a, b) {
+    this.cards.sort(function(a,b) { // .sort permet le tri mais je ne comprend pas l'importance des paramètres. Nous trions les objets de l'array "card"
       // Math.random() // ]0; 1[, ex: 0.123456
 
-      if (Math.random() > .5) {
+      if (Math.random() > .5) { // on créer une condition pour que le Math.random renvoie un nombre entier
         return 1
       } else {
         return -1
       }
-    })
-  }
+    });
+  };
 
 
   checkIfPair(card1, card2) {
-    this.pairsClicked++
-    if (card1 === card2) {
-      this.pairsGuessed++
+    this.pairsClicked++ // dès que je click sur 2 cartes la condition ci-dessous s'applique...
+    if (card2 === card1){
+      this.pairsGuessed++ // on valide le fait d'avoir une paire et on ajoute 1 à la propriété this.pairsGuessed
       return true
     } else {
       return false
     }
+  };
 
-
-
-  }
 
   checkIfFinished() {
-    // ... write your code here
-  }
-}
+    if (this.pairsGuessed === this.cards.length / 2){ // la condition est que si j'ai trouvé la moitié de l'ensemble des cartes, celà induit que j'ai également trouvé son équivalent et donc réussi le memoru game
+      return true
+      } else {
+      return false
+      }
+    };
+};
+
 
 // The following is required for automated testing. Please, ignore it.
 if (typeof module !== 'undefined') module.exports = MemoryGame;
